4b4541a35569c7ae763470fa7e531da493d4c5d2
RadhityaArdh <radhityaardhi388@gmail.com>
2026-01-20 19:04:27 +0700

    benerin redirect

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,7 +11,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        // Daftarkan middleware alias kita di sini
+        $middleware->trustProxies(at: '*');
         $middleware->alias([
             'role' => \App\Http\Middleware\CheckRole::class,
         ]);
